28fae95fd343abb045f5ca829f77bfa48c57ebbd
Robson <robmartinhao@gmail.com>
2021-10-07 21:10:46 -0300

    Projeto inicial

---
+++ src/main/java/com/algaworks/algafoodfundamentos/AlgafoodFundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodfundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodFundamentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodFundamentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algaworks/algafoodfundamentos/AlgafoodFundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodfundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodFundamentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
